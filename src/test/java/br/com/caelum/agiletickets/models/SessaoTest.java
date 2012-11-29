9ec859cafc73a5256b3a538e949052eb3b4b92b7
Ana <ana.cavalcanti@mpt.gov.br>
2012-11-29 14:26:29 -0200

    Adicionou outros testes na SessaoTest

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -13,6 +13,26 @@ public class SessaoTest {
 
         Assert.assertTrue(sessao.podeReservar(1));
 	}
+	
+	public void naoDeveVenderZeroIngresso(){
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(1);
+		
+		Assert.assertFalse(sessao.podeReservar(0));
+	}
+	
+	public void naoDeveVenderQtdNegativa(){
+		Sessao sessao = new Sessao();
+		Assert.assertFalse(sessao.podeReservar(-1));
+	}
+	
+	@Test
+	public void deveVender1ingressoSeHa1vaga(){
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(1);
+		
+		Assert.assertTrue(sessao.podeReservar(1));
+	}
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
